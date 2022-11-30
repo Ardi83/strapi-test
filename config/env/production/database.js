8ad0064db41b9619c5c806d405b89cca0b05758a
AD\arvid.nasirabadi <arvid.nasirabadi@phosworks.com>
2022-11-30 17:07:59 +0100

    install pg is compolsory for render web service

--- config/env/production/database.js
+++ config/env/production/database.js
@@ -1,13 +1,18 @@
-module.exports = ({ env }) => ({
-    connection: {
-      client: 'postgres',
-      connection: {
-        host: env('DATABASE_HOST', process.env.DATABASE_HOST),
-        port: env.int('DATABASE_PORT', process.env.DATABASE_PORT),
-        database: env('DATABASE_NAME', process.env.DATABASE_NAME),
-        user: env('DATABASE_USERNAME', process.env.DATABASE_USERNAME),
-        password: env('DATABASE_PASSWORD', process.env.DATABASE_PASSWORD),
-      },
-      debug: false,
-    },
-  });
+const { parse } = require('pg-conncection-string');
+
+module.exports = ({ env }) => {
+    const { host, port, database, user, password } = parse(env('DATABASE_URL'));
+    return {
+        connection: {
+            client: 'postgres',
+            connection: {
+                host,
+                port,
+                database,
+                user,
+                password,
+            },
+            debug: false,
+        },
+    }
+  };
